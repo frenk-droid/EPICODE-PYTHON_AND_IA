2a1eb11ec549c0d80abde64a1d4b293ff7b7387b
frenk-droid <teofrenk99@gmail.com>
2026-01-10 23:53:04 +0100

    Esercizi su casting

---
+++ MODULO_1/casting.py
@@ -0,0 +1,6 @@
+numero= input("Inserisci un numero intero: ")
+numero_flt= float(numero)
+print("""Il numero stampato come float è """
+      f"""{numero_flt}""")
+print(f"Il numero come stringa è {str(numero_flt)}")
+
