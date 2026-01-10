77703ebc4af6e013ad516584e950e4ef902b0da8
frenk-droid <teofrenk99@gmail.com>
2026-01-10 17:19:22 +0100

    Esercizio sui tipi di dato
    EPICODE MODULO 1

---
+++ MODULO_1/tipi_di_dati.py
@@ -0,0 +1,19 @@
+#Esercizio uno
+x = 7
+y = 3
+print(x + y)
+
+#Esercizio due
+prezzo = 19.99
+quantità = 3
+print(prezzo * quantità)
+
+#Esercizio tre
+numero = 10
+print(numero > 5)
+
+a = 5
+b = 2.5
+a, b = b, a
+print(a, b)
+print(a + b)
