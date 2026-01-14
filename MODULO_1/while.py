441c28e1ab7a7a41b28ca3d2411d3d25f88e980d
frenk-droid <teofrenk99@gmail.com>
2026-01-14 22:01:21 +0100

    Aggiunta esercizio while

--- MODULO_1/while.py
+++ MODULO_1/while.py
@@ -1,6 +1,18 @@
+#esercizio uno
 numero= -1
 
 while numero  <= 0:
-    numero=int(input("Inserisci un numero positivo"))
+   numero=int(input("Inserisci un numero positivo"))
 
 print(f"Hai inserito il numero positivo {numero}")
+
+#esercizio due
+numero = 123456789
+somma=0
+
+while numero != 0:
+    print(numero%10,numero // 10)
+    somma += numero % 10
+    numero= numero // 10
+
+print(somma)
